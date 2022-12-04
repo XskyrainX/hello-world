e6e51ec931daaca028878b573a286bb1911094c1
XskyrainX <yrymikusuki@gmail.com>
2022-12-04 20:18:08 +0800

    mistake

--- C Primer Plus/Chapter II/2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    printf("YuRunyu QingDao");
-    
-    return 0;
-}
--- C Primer Plus/Chapter II/3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int age = 22;
-
-    printf("My age: %d, which is %d days", age, age * 365);
-    return 0;
-}
